7cf25828984b6c1a4256dd1c1e2f2b830c551f4e
Гондурова Дарья <54889938+gondurovad@users.noreply.github.com>
2021-11-04 22:13:41 +0300

    override equals hashcode

--- src/com/besmart/hw1/part1/Rectangle.java
+++ src/com/besmart/hw1/part1/Rectangle.java
@@ -40,4 +40,25 @@ public class Rectangle {
     public String toString() {
         return "Rectangle[length="+length+", width="+width+"]";
     }
+    
+    @Override
+    public int hashCode() {
+        int result = 17;
+
+        result = 31*result + Float.floatToIntBits(length);
+        result = 31*result + Float.floatToIntBits(width);
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+
+        Rectangle rect = (Rectangle) obj;
+
+        return this.length == rect.getLength() && this.width == rect.getWidth();
+    }
 }
